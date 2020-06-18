98810b6ad2ad539ebf76a22c94c35d13a0955d27
Sean Harkins <sharkinsgis@gmail.com>
2020-06-18 17:47:11 -0500

    Organize test and setuptools structure correctly.

--- setup.py
+++ setup.py
@@ -5,17 +5,12 @@ setup(
     version="0.1",
     packages=find_packages(exclude=["tests"]),
     include_package_data=True,
-    install_requires=[
-        "numpy",
-        "gdal",
-        "xmltodict",
-        "click",
-    ],
-    tests_require=["pytest"],
+    install_requires=["numpy", "gdal", "xmltodict", "click",],
+    extras_require={"testing": ["pytest"]},
     package_data={"hls_browse_imagery_creator": ["data/*.json"]},
-    setup_requires=['flake8', 'pytest-runner'],
-    entry_points='''
+    setup_requires=["flake8"],
+    entry_points="""
         [console_scripts]
         granule_to_gibs=hls_browse_imagery_creator.granule_to_gibs:granule_to_gibs
-    ''',
+    """,
 )
