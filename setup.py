f26d362b38eadae46ba4dec7373dfa24527913c6
Sean Harkins <sharkinsgis@gmail.com>
2020-06-23 14:01:25 -0500

    Include create_gibs_tile to merge sub tile tiffs.

--- setup.py
+++ setup.py
@@ -5,12 +5,13 @@ setup(
     version="0.1",
     packages=find_packages(exclude=["tests"]),
     include_package_data=True,
-    install_requires=["numpy", "gdal", "xmltodict", "click",],
+    install_requires=["numpy", "gdal", "xmltodict", "click", ],
     extras_require={"testing": ["pytest"]},
     package_data={"hls_browse_imagery_creator": ["data/*.json"]},
     setup_requires=["flake8"],
     entry_points="""
         [console_scripts]
         granule_to_gibs=hls_browse_imagery_creator.granule_to_gibs:granule_to_gibs
+        create_gibs_tile=hls_browse_imagery_creator.create_gibs_tile:create_gibs_tile
     """,
 )
